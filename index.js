b28b1f96acb8a1596312e327269572134c9db446
aberdeen43 <89466726+aberdeen43@users.noreply.github.com>
2021-11-26 21:50:31 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,28 +1,9 @@
-const { ApolloServer } = require("apollo-server");
-const { ApolloServerPluginLandingPageGraphQLPlayground } = require("apollo-server-core");
-const mongoose = require("mongoose");
+const listResolvers = require("./list");
+const descResolvers = require("./desc");
+const cartResolvers = require("./cart");
 
-const typeDefs = require("./graphql/typeDefs.js");
-const resolvers = require("./graphql/resolvers")
-const { MONGODB } = require("./config.js");
-
-const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    plugins: [
-        ApolloServerPluginLandingPageGraphQLPlayground
-    ]
-});
-
-mongoose.connect(MONGODB, { useNewUrlParser: true })
-    .then(() => {
-        console.log("database connected")
-        return server.listen({ port: 5000 });
-    },
-    (reason) => {
-        console.log(`reason is ${reason}`);
-        return reason;
-    })
-    .then((res) => {
-        console.log(`server running at ${res.url}`);
-    })
+module.exports = {
+    Query: {
+        ...listResolvers.Query
+    }
+}
